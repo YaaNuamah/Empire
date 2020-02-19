70b034a06598a1ac528ca15fb79a1f378544f15d
YaaNuamah <yaa_fordjour@hotmail.com>
2020-02-19 17:58:28 +0000

    generated springboot

---
+++ src/main/java/io/turntabl/Empire/EmpireApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.Empire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/Empire/EmpireApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.Empire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e97958a953c10d897c35c3e610c21a3d61b5604a
YaaNuamah <yaa_fordjour@hotmail.com>
2020-04-29 19:28:22 +0000

    updated project controller to accept project_ids

--- src/main/java/io/turntabl/Empire/Controllers/ProjectController.java
+++ src/main/java/io/turntabl/Empire/Controllers/ProjectController.java
@@ -5,9 +5,13 @@ import io.swagger.annotations.ApiOperation;
 import io.turntabl.Empire.models.ProjectTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Api
@@ -19,11 +23,21 @@ public class ProjectController {
     @CrossOrigin
     @ApiOperation("Add a Project")
     @PostMapping("/api/v1/addNewProject")
-    public void addNewProject(@RequestBody ProjectTO project) {
-        template.update(
-                "insert into turntabl_project(project_name) values(?)",
-                project.getProject_name()
-        );
+    public Object addNewProject(@RequestBody ProjectTO project) {
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(template)
+                .withTableName("turntabl_project")
+                .usingGeneratedKeyColumns("project_id");
+        Map<String, Object> parameters = new HashMap<>();
+        Object project_name = new Object();
+        parameters.put("project_name",project.getProject_name());
+
+        Number key = insert.executeAndReturnKey(parameters);
+
+        Map<String, Object> results = new HashMap<>();
+        results.put("key", key.intValue());
+
+        return results;
+
     }
 
     @CrossOrigin
@@ -39,32 +53,12 @@ public class ProjectController {
     @CrossOrigin
     @ApiOperation("Get a Project By Id")
     @GetMapping("/api/v1/project/{id}")
-    public ProjectTO viewProjectById(@PathVariable ("id") Integer id) {
+    public ProjectTO viewProjectById(@PathVariable("id") Integer id) {
         return (ProjectTO) template.queryForObject(
                 "select project_id, project_name from turntabl_project where project_id = ?",
                 new Object[]{id},
                 new BeanPropertyRowMapper<>(ProjectTO.class)
         );
     }
-
-
-//    @CrossOrigin
-//    @PatchMapping("/api/v1/project/{id}")
-//    public void updateProject(@RequestBody ProjectTO project, @PathVariable("id") String id) {
-//        return ProjectTO
-//    }
-//    @CrossOrigin
-//    @PatchMapping("/api/v1/stories/{id}")
-//    public void updateStory(@RequestBody StoryTO uStory, @PathVariable("id") String id) {
-//        StoryTO story = db.find(StoryTO.class, id);
-//
-//        story.setAuthor_id(uStory.getAuthor_id());
-//        story.setStory_title(uStory.getStory_title());
-//        story.setStory_description(uStory.getStory_description());
-//        story.setCategory_id(uStory.getCategory_id());
-//        story.setStory_photo(uStory.getStory_photo());
-//
-//        Response response = db.update(story);
-//    }
 
 }
